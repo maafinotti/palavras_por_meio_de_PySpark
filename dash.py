e614a399a219e1ac43a0dbbca2e619b9b7a22dfb
Manoela Finotti <106084544+maafinotti@users.noreply.github.com>
2024-06-19 10:33:45 -0300

    Add files via upload

--- dash.py
+++ dash.py
@@ -14,76 +14,75 @@ def main():
         dfs = {sheet_name: xls.parse(sheet_name) for sheet_name in xls.sheet_names}
         return dfs
 
-    with st.sidebar:
-        st.title("Dashboard - Espumante")
-        uploaded_file = st. file_uploader("Coloque o seu arquivo 'sites.xlsx' aqui")
-    if uploaded_file is None:
-        st.title(f"Adicione o arquivo Excel 'sites.xlsx' na sidebar")
-    else:
-        dfs = load_data(uploaded_file)
+    # with st.sidebar:
+    #     st.title("Dashboard - Espumante")
+    #     uploaded_file = st. file_uploader("Coloque o seu arquivo 'sites.xlsx' aqui")
+    # if uploaded_file is None:
+    #     st.title(f"Adicione o arquivo Excel 'sites.xlsx' na sidebar")
+    # else:
+    dfs = load_data('sites.xlsx')
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image("espumante.png", width=70)
+    with col2:
+        st.image("tacas.png", width=70)
 
-        col1, col2 = st.columns([1, 2])
-        with col1:
-            st.image("espumante.png", width=70)
-        with col2:
-            st.image("tacas.png", width=70)
+    # Título da página
+    st.title('Dashboard - Espumante')
 
-        # Título da página
-        st.title('Dashboard - Espumante')
+    # Botão para exibir as palavras utilizadas na pesquisa
+    state = st.button('Clique aqui para ver as palavras utilizadas na pesquisa')
+    if state:
+        if st.button('Clique aqui para ocultar'):
+            state = False
+        else:
+            st.write("""
+            espumantes, champagne,  adega,  prosecco, casamento,  casamentos, uvas, uva,  vinícula, diversão, bebida, festas, festa,  jantar, jantares, gastronomia,  degustação, queijo, chocolate,  chocolates, vinho,  vinhos, enoturismo, vinhedo,  taças,  taça, restaurante,  restaurantes, brinde, drink,  sabor , culinária,  massas, bruschettas,  frança, itália, espanha,  anonovo,  reveillon,  frescor,  sofisticação, cítricas, cítrico,  refrescante,  comidas , aperitivos, sobremesas, frutado,  chardonnay ,  flores, joias,  presentes,  presente, cestas, peixe,  peixes, marisco,  mariscos, acidez, cerimonialista, decoração,  fotografia, música, bartenders, bar,  bares,  confeitaria,  doce, doces,  arranjos, frisante, rótulos , salton, chandon,  freixenet,  garibaldi , aurora, miolo,  noiva,  noivas, safra, brunch, vinho espumante,  happy hour, espumante nature, espumante extrabrut,  espumante brut, espumante sec,  espumante seco, espumante demisec,  espumante doce, sabor frutado,  frutos do mar,  vinho frisante, ano novo, cave geisse,  casa perini,  casa valduga, vinícola aurora
+            """)
 
-        # Botão para exibir as palavras utilizadas na pesquisa
-        state = st.button('Clique aqui para ver as palavras utilizadas na pesquisa')
-        if state:
-            if st.button('Clique aqui para ocultar'):
-                state = False
-            else:
-                st.write("""
-                espumantes, champagne,  adega,  prosecco, casamento,  casamentos, uvas, uva,  vinícula, diversão, bebida, festas, festa,  jantar, jantares, gastronomia,  degustação, queijo, chocolate,  chocolates, vinho,  vinhos, enoturismo, vinhedo,  taças,  taça, restaurante,  restaurantes, brinde, drink,  sabor , culinária,  massas, bruschettas,  frança, itália, espanha,  anonovo,  reveillon,  frescor,  sofisticação, cítricas, cítrico,  refrescante,  comidas , aperitivos, sobremesas, frutado,  chardonnay ,  flores, joias,  presentes,  presente, cestas, peixe,  peixes, marisco,  mariscos, acidez, cerimonialista, decoração,  fotografia, música, bartenders, bar,  bares,  confeitaria,  doce, doces,  arranjos, frisante, rótulos , salton, chandon,  freixenet,  garibaldi , aurora, miolo,  noiva,  noivas, safra, brunch, vinho espumante,  happy hour, espumante nature, espumante extrabrut,  espumante brut, espumante sec,  espumante seco, espumante demisec,  espumante doce, sabor frutado,  frutos do mar,  vinho frisante, ano novo, cave geisse,  casa perini,  casa valduga, vinícola aurora
-                """)
+    # Seleção do site
+    site_selecionado = st.selectbox('Selecione o site', list(dfs.keys()))
 
-        # Seleção do site
-        site_selecionado = st.selectbox('Selecione o site', list(dfs.keys()))
+    # Cabeçalho
+    st.header(f'Palavras que mais aparecem no site: **{site_selecionado}**')
 
-        # Cabeçalho
-        st.header(f'Palavras que mais aparecem no site: **{site_selecionado}**')
+    st.write("")
 
-        st.write("")
+    # Dados do site selecionado
+    data = dfs[site_selecionado]
 
-        # Dados do site selecionado
-        data = dfs[site_selecionado]
+    # # Nuvem de palavras do site
+    word_freq = dict(zip(data['palavra'], data['qtde']))
 
-        # # Nuvem de palavras do site
-        word_freq = dict(zip(data['palavra'], data['qtde']))
+    # Gerar a Word Cloud
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
     
-        # Gerar a Word Cloud
-        wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
-        
-        # Plotar a Word Cloud
-        plt.figure(figsize=(10, 5))
-        plt.imshow(wordcloud, interpolation='bilinear')
-        plt.axis('off')
-        plt.title(f'Word Cloud para {site_selecionado}')
-        plt.show()
-        st.pyplot(plt)
+    # Plotar a Word Cloud
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    plt.title(f'Word Cloud para {site_selecionado}')
+    plt.show()
+    st.pyplot(plt)
 
-        st.write("")
-        st.write("")
+    st.write("")
+    st.write("")
 
-        col1, col2 = st.columns([1, 2])
+    col1, col2 = st.columns([1, 2])
 
-        with col1:
-            # Dataframe com as palavras do site
-            st.write(f"Palavras do site: **{site_selecionado}**")
-            st.dataframe(data)
+    with col1:
+        # Dataframe com as palavras do site
+        st.write(f"Palavras do site: **{site_selecionado}**")
+        st.dataframe(data)
 
-        with col2:
-            # Gráfico interativo com Plotly Express
-            st.write("Gráfico de Frequência das Palavras:")
-            fig = px.bar(data, x='palavra', y='qtde', title=f'Frequência de Palavras no site {site_selecionado}', 
-                        labels={'palavra': 'Palavra', 'qtde': 'Quantidade'},
-                        template='plotly_white')
-            fig.update_layout(xaxis_tickangle=-45)
+    with col2:
+        # Gráfico interativo com Plotly Express
+        st.write("Gráfico de Frequência das Palavras:")
+        fig = px.bar(data, x='palavra', y='qtde', title=f'Frequência de Palavras no site {site_selecionado}', 
+                    labels={'palavra': 'Palavra', 'qtde': 'Quantidade'},
+                    template='plotly_white')
+        fig.update_layout(xaxis_tickangle=-45)
 
-            st.plotly_chart(fig, use_container_width=True)
+        st.plotly_chart(fig, use_container_width=True)
 if __name__  == '__main__':
     main()
