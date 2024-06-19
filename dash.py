072ade1d43cc0ae30e50d4897c58f25b2fa30a12
Manoela Finotti <106084544+maafinotti@users.noreply.github.com>
2024-06-18 22:00:55 -0300

    Add files via upload

--- dash.py
+++ dash.py
@@ -5,74 +5,79 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from PIL import Image
 
-@st.cache_data
-def load_data(file):
-    xls = pd.ExcelFile(file)
-    dfs = {sheet_name: xls.parse(sheet_name) for sheet_name in xls.sheet_names}
-    return dfs
 
-def gerar_nuvem_de_palavras(data):
-    text = ' '.join([f"{row['palavra']} " * int(row['qtde']) for _, row in data.iterrows()])
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
-    return wordcloud
+def main():
+    @st.cache_data
+    def load_data(file):
+        xls = pd.ExcelFile(file)
+        dfs = {sheet_name: xls.parse(sheet_name) for sheet_name in xls.sheet_names}
+        return dfs
 
-# Carregar os dados
-dfs = load_data('sites.xlsx')
+    def gerar_nuvem_de_palavras(data):
+        text = ' '.join([f"{row['palavra']} " * int(row['qtde']) for _, row in data.iterrows()])
+        wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+        return wordcloud
 
-col1, col2 = st.columns([1, 2])
-with col1:
-    st.image("espumante.png", width=70)
-with col2:
-    st.image("tacas.png", width=70)
+    # Carregar os dados
+    dfs = load_data('sites.xlsx')
 
-# Título da página
-st.title('Dashboard - Espumante')
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.image("espumante.png", width=70)
+    with col2:
+        st.image("tacas.png", width=70)
 
-# Botão para exibir as palavras utilizadas na pesquisa
-state = st.button('Clique aqui para ver as palavras utilizadas na pesquisa')
-if state:
-    if st.button('Clique aqui para ocultar'):
-        state = False
-    else:
-        st.write("""
-        espumantes, champagne,  adega,  prosecco, casamento,  casamentos, uvas, uva,  vinícula, diversão, bebida, festas, festa,  jantar, jantares, gastronomia,  degustação, queijo, chocolate,  chocolates, vinho,  vinhos, enoturismo, vinhedo,  taças,  taça, restaurante,  restaurantes, brinde, drink,  sabor , culinária,  massas, bruschettas,  frança, itália, espanha,  anonovo,  reveillon,  frescor,  sofisticação, cítricas, cítrico,  refrescante,  comidas , aperitivos, sobremesas, frutado,  chardonnay ,  flores, joias,  presentes,  presente, cestas, peixe,  peixes, marisco,  mariscos, acidez, cerimonialista, decoração,  fotografia, música, bartenders, bar,  bares,  confeitaria,  doce, doces,  arranjos, frisante, rótulos , salton, chandon,  freixenet,  garibaldi , aurora, miolo,  noiva,  noivas, safra, brunch, vinho espumante,  happy hour, espumante nature, espumante extrabrut,  espumante brut, espumante sec,  espumante seco, espumante demisec,  espumante doce, sabor frutado,  frutos do mar,  vinho frisante, ano novo, cave geisse,  casa perini,  casa valduga, vinícola aurora
-        """)
+    # Título da página
+    st.title('Dashboard - Espumante')
 
-# Seleção do site
-site_selecionado = st.selectbox('Selecione o site', list(dfs.keys()))
+    # Botão para exibir as palavras utilizadas na pesquisa
+    state = st.button('Clique aqui para ver as palavras utilizadas na pesquisa')
+    if state:
+        if st.button('Clique aqui para ocultar'):
+            state = False
+        else:
+            st.write("""
+            espumantes, champagne,  adega,  prosecco, casamento,  casamentos, uvas, uva,  vinícula, diversão, bebida, festas, festa,  jantar, jantares, gastronomia,  degustação, queijo, chocolate,  chocolates, vinho,  vinhos, enoturismo, vinhedo,  taças,  taça, restaurante,  restaurantes, brinde, drink,  sabor , culinária,  massas, bruschettas,  frança, itália, espanha,  anonovo,  reveillon,  frescor,  sofisticação, cítricas, cítrico,  refrescante,  comidas , aperitivos, sobremesas, frutado,  chardonnay ,  flores, joias,  presentes,  presente, cestas, peixe,  peixes, marisco,  mariscos, acidez, cerimonialista, decoração,  fotografia, música, bartenders, bar,  bares,  confeitaria,  doce, doces,  arranjos, frisante, rótulos , salton, chandon,  freixenet,  garibaldi , aurora, miolo,  noiva,  noivas, safra, brunch, vinho espumante,  happy hour, espumante nature, espumante extrabrut,  espumante brut, espumante sec,  espumante seco, espumante demisec,  espumante doce, sabor frutado,  frutos do mar,  vinho frisante, ano novo, cave geisse,  casa perini,  casa valduga, vinícola aurora
+            """)
 
-# Cabeçalho
-st.header(f'Palavras que mais aparecem no site: **{site_selecionado}**')
+    # Seleção do site
+    site_selecionado = st.selectbox('Selecione o site', list(dfs.keys()))
 
-st.write("")
+    # Cabeçalho
+    st.header(f'Palavras que mais aparecem no site: **{site_selecionado}**')
 
-# Dados do site selecionado
-data = dfs[site_selecionado]
+    st.write("")
 
-# Nuvem de palavras do site
-st.write(f"Nuvem de Palavras do site: **{site_selecionado}**")
-wordcloud = gerar_nuvem_de_palavras(dfs[site_selecionado])
-plt.figure(figsize=(10, 5))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-st.pyplot(plt)
+    # Dados do site selecionado
+    data = dfs[site_selecionado]
 
-st.write("")
-st.write("")
+    # Nuvem de palavras do site
+    st.write(f"Nuvem de Palavras do site: **{site_selecionado}**")
+    wordcloud = gerar_nuvem_de_palavras(dfs[site_selecionado])
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot(plt)
 
-col1, col2 = st.columns([1, 2])
+    st.write("")
+    st.write("")
 
-with col1:
-    # Dataframe com as palavras do site
-    st.write(f"Palavras do site: **{site_selecionado}**")
-    st.dataframe(data)
+    col1, col2 = st.columns([1, 2])
 
-with col2:
-    # Gráfico interativo com Plotly Express
-    st.write("Gráfico de Frequência das Palavras:")
-    fig = px.bar(data, x='palavra', y='qtde', title=f'Frequência de Palavras no site {site_selecionado}', 
-                labels={'palavra': 'Palavra', 'qtde': 'Quantidade'},
-                template='plotly_white')
-    fig.update_layout(xaxis_tickangle=-45)
+    with col1:
+        # Dataframe com as palavras do site
+        st.write(f"Palavras do site: **{site_selecionado}**")
+        st.dataframe(data)
 
-    st.plotly_chart(fig, use_container_width=True)
+    with col2:
+        # Gráfico interativo com Plotly Express
+        st.write("Gráfico de Frequência das Palavras:")
+        fig = px.bar(data, x='palavra', y='qtde', title=f'Frequência de Palavras no site {site_selecionado}', 
+                    labels={'palavra': 'Palavra', 'qtde': 'Quantidade'},
+                    template='plotly_white')
+        fig.update_layout(xaxis_tickangle=-45)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+if __name__  == '__main__':
+    main()
